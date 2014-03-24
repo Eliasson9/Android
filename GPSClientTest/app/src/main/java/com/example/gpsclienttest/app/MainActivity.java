5029ee32af2f94e97445dd0ce4a32153bc67ef11
patrik <patrik@patrik-Lenovo-IdeaPad-Y510P.(none)>
2014-03-24 13:29:18 +0100

    added peremission

--- GPSClientTest/app/src/main/java/com/example/gpsclienttest/app/MainActivity.java
+++ GPSClientTest/app/src/main/java/com/example/gpsclienttest/app/MainActivity.java
@@ -6,12 +6,15 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class MainActivity extends Activity {
@@ -24,7 +27,7 @@ public class MainActivity extends Activity {
     }
 
     //Connect to server when clicking the connectButton
-    public void onClick_connect() {
+    public void onClick_connect(View v) {
         ClientTask client = new ClientTask();
         client.execute();
     }
@@ -69,8 +72,11 @@ public class MainActivity extends Activity {
         protected Void doInBackground(Void... params) {
             try {
                 socket = new Socket("localhost", 8080);
-
-            } catch (Exception e) {
+            }
+            catch (UnknownHostException e) {
+                System.out.println(e);
+            }
+            catch (Exception e) {
                 System.out.println(e);
             }
 
